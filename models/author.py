6f2f5fe31d4778ca82c82958d49cf242e0d807da
Allison Blumenthal <blumen.allison@gmail.com>
2023-05-04 22:15:41 -0400

    added classes for author and book

---
+++ models/author.py
@@ -0,0 +1,9 @@
+class Author():
+  def __init__(self, id, email, first_name, last_name, image, favorite):
+    self.id = id
+    self.email = email
+    self.first_name = first_name
+    self.last_name = last_name
+    self.image = image
+    self.favorite = favorite
+    
---
+++ models/book.py
@@ -0,0 +1,9 @@
+class Book():
+  def __init__(self, id, title, image, price, sale, description):
+    self.id = id
+    self.title = title
+    self.image = image
+    self.price = price
+    self.sale = sale
+    self.description = description
+    
